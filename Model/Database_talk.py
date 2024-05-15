4dbcc174cef42a2de4edb64f6f316d25c67ba5e1
JJtheron <theronjanjacques@gmail.com>
2024-05-15 16:59:54 -0700

    Addid my first moddle class

--- Model/Database_talk.py
+++ Model/Database_talk.py
@@ -1,25 +1,9 @@
-from threading import Thread
 
-from time import sleep
-{"Name": "JJ", "Company Name": "JJ Co", "Person Visiting": "no one", "Phone Number": "0563165194", "TimeIn":"10/65/32 213464", "TimeOut":"10/65/32 213464"}
-class Write_data_to_disk(Thread):
-    def __init__(self,q):
-        super().__init__()
-        self.queue = q
 
-    def run(self):
-        try:           
-            self.queue.put("Data_written")
-        finally:
-            return "fail"
-
-class Read_data_from_disk(Thread):
-    def __init__(self,q):
-        super().__init__()
-        self.queue = q  
-
-    def run(self):
-        try:           
-            self.queue.put("Data_That_was_read")
-        finally:
-            return "fail"
+class Read_write_data():
+    def __init__(self,host,user,password,database,table):
+        self.host = host
+        self.user = user
+        self.password  = password
+        self.database = database
+        self.table = table
