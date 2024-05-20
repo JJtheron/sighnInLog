429c0dbc6e7f39af1c5c6ed9df2f399f4c252f46
JJtheron <theronjanjacques@gmail.com>
2024-05-19 21:31:43 -0700

    Can put stuff in the database. Get a key back to reference back to the line.

--- Model/Database_talk.py
+++ Model/Database_talk.py
@@ -1,11 +1,20 @@
-
+import mysql.connector
+from datetime import datetime
 
 class Read_write_data():
-    def __init__(self,host,user,password,database):
-        self.host = host
+    def __init__(self,host1,user,password,database):
+        self.host = host1
         self.user = user
         self.password  = password
         self.database = database
+        self.mydb = mysql.connector.connect(
+            host=host1,
+            user=self.user,
+            password=self.password,
+            database=self.database,
+            auth_plugin='mysql_native_password'
+        )
+        self.mycursor = self.mydb.cursor()
 
     def get_last_500(self):
         #get the last 500 records and return in a dictionary
@@ -17,8 +26,20 @@ class Read_write_data():
         return rec_1
     
     def add_record(self,rec_add):
-        key = "1234"
-        return key
+        sql = "INSERT INTO sign_in_sheet (name, time_in, time_out, company, visiting, phone_number) VALUES (%s,%s,%s,%s,%s,%s)"
+        val = (rec_add["name"],rec_add["time_in"], rec_add["time_out"],rec_add["company_name"], rec_add["visiting_who"],rec_add["phone"])
+        sqlshow = f"""SELECT  en_id from sign_in_sheet where 
+        name='{rec_add["name"]}' AND
+        time_in='{rec_add["time_in"]}' AND
+        time_out='{rec_add["time_out"]}' AND
+        company='{rec_add["company_name"]}' AND
+        visiting='{rec_add["visiting_who"]}' AND
+        phone_number='{rec_add["phone"]}'
+        """
+        #self.mycursor.execute(sql, val)
+        #self.mydb.commit()
+        self.mycursor.execute(sqlshow)
+        return self.mycursor.fetchall()[0][0]
     
     def change_record(self,key,new_record):
         return key
@@ -26,7 +47,7 @@ class Read_write_data():
     def look_up_user_info(self,tag_id):
         user_rec = {"tag_id":"","name":"","company_name":"","phone":""}
         return user_rec
-
+    
     def del_user_info(self,tag_id):
         return tag_id
     
@@ -37,12 +58,12 @@ class Read_write_data():
 if __name__ == "__main__":
     print("")
     print("++++++++++++++++++++++++++++++populate database and look at keys++++++++++++++++++++++++")
-    WRdata = Read_write_data("localhost","ui_user","Copy_is_in_Hell","checkin")
-    newkey = WRdata.add_record({"name":"JJ","time_in":"'2007-12-31 23:59:59","time_out":"2024-12-31 23:59:59","company_name":"hardCore","visiting_who":"myself","phone":"4582212784"})
-    newkey1 = WRdata.add_record({"name":"JJ1","time_in":"'2007-12-31 23:59:00","time_out":"2024-12-31 23:59:03","company_name":"hardCore","visiting_who":"myself","phone":"4582212784"})
-    newkey2 = WRdata.add_record({"name":"JJ2","time_in":"'2007-12-31 23:59:01","time_out":"2024-12-31 23:59:02","company_name":"hardCore","visiting_who":"myself","phone":"4582212784"})
-    newkey3 = WRdata.add_record({"name":"JJ3","time_in":"'2007-12-31 23:59:02","time_out":"2024-12-31 23:59:01","company_name":"hardCore","visiting_who":"myself","phone":"4582212784"})
-    newkey4 = WRdata.add_record({"name":"JJ4","time_in":"'2007-12-31 23:59:03","time_out":"2024-12-31 23:59:00","company_name":"hardCore","visiting_who":"myself","phone":"4582212784"})
+    WRdata = Read_write_data("127.0.0.1","root","Copy_is_evel","sighn_in_sighn_out")
+    newkey = WRdata.add_record({"name":"JJ","time_in":"2007-12-31 23:59:59","time_out":"2024-12-31 23:59:59","company_name":"hardCore","visiting_who":"myself","phone":"4582212784"})
+    newkey1 = WRdata.add_record({"name":"JJ1","time_in":"2007-12-31 23:59:00","time_out":"2024-12-31 23:59:03","company_name":"hardCore","visiting_who":"myself","phone":"4582212784"})
+    newkey2 = WRdata.add_record({"name":"JJ2","time_in":"2007-12-31 23:59:01","time_out":"2024-12-31 23:59:02","company_name":"hardCore","visiting_who":"myself","phone":"4582212784"})
+    newkey3 = WRdata.add_record({"name":"JJ3","time_in":"2007-12-31 23:59:02","time_out":"2024-12-31 23:59:01","company_name":"hardCore","visiting_who":"myself","phone":"4582212784"})
+    newkey4 = WRdata.add_record({"name":"JJ4","time_in":"2007-12-31 23:59:03","time_out":"2024-12-31 23:59:00","company_name":"hardCore","visiting_who":"myself","phone":"4582212784"})
     print(newkey)
     print(newkey1)
     print(newkey2)
